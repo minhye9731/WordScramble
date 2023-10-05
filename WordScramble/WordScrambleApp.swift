e36f04ba42c0203413a7d658f41fad95601c581a
minhye9731 <jenney62639731@gmail.com>
2023-10-05 23:39:54 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by 강민혜 on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
