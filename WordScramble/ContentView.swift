432b9c6287c16f23a2c1d6bad474851bc8ba14f5
minhye9731 <jenney62639731@gmail.com>
2023-10-07 17:03:35 +0900

    feat : practice trimmingCharacters, UITextChecker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let people = ["Finn", "Leia", "Luke", "Rey"]
     var body: some View {
         VStack {
             Image(systemName: "globe")
